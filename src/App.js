2262b398b0bd1e845deb3989dd87a76ee3c598d4
Jatin Kumar <109254658+jatinkumar300403@users.noreply.github.com>
2023-05-02 04:30:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import { useState, useEffect } from "react"
 import Loading from "./Loading"
 import Profile from "./Profile"
 
-// const singleUser = `https://api.github.com/users/SankThomas`
-// const repos = `https://api.github.com/users/sankthomas/repos?per_page=5`
-// https://api.github.com/users/sankthomas/repos?page=1&per_page=10&sort=updated
-
 function App() {
   const [items, setItems] = useState([])
-  // Change this to any username whose repositories you want to get
   const [user] = useState("jatinkumar300403")
 
   useEffect(() => {
